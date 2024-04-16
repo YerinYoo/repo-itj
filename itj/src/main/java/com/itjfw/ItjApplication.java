3a1e1c255d5a6a7ad646eca9f62361485de73752
talstrs <tal95@daum.net>
2024-04-16 15:13:01 +0900

    first

---
+++ itj/src/main/java/com/itjfw/ItjApplication.java
@@ -0,0 +1,13 @@
+package com.itjfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItjApplication.class, args);
+	}
+
+}
